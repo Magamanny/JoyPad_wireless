38b196dbc5e83e601010a80c918ff00caa653215
megamanny <magamanny@gmail.com>
2019-08-19 19:59:43 +0500

    the code is working, needed a 100uf cap at the 3.3v supply by the nrf24l01

--- nrf24MyTest/nrf24MyTest.ino
+++ nrf24MyTest/nrf24MyTest.ino
@@ -18,27 +18,34 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(7, 8); // CE, CSN
+RF24 radio(7, 6); // CE, CSN
 
 const byte address[6] = "00001";
 
 void setup() {
   Serial.begin(9600);
-  pinMode(8, INPUT);
+  pinMode(8, INPUT); // button pin
   while(!Serial)
   ;
   radio.begin();
   radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
+  radio.setPALevel(RF24_PA_LOW);
   radio.stopListening();
+  Serial.println("NRF24L01 Test.");
   Serial.print("isChipConnected()=");
   Serial.println(radio.isChipConnected());
-  Serial.println("Hello World!");
+  Serial.print("pip address=");
+  for(int i=0;i<6;i++)
+    Serial.print((char)address[i]); // as the print function refuese to print the const char type
+  Serial.println();
+  Serial.println("This is the transmitter");
 }
 
 void loop() {
   const char text[] = "Hello World";
   radio.write(&text, sizeof(text));
+  Serial.print("Sending data=");
+  Serial.println(text);
   delay(1000);
   Serial.print("pin 8=");
   Serial.println(digitalRead(8));
